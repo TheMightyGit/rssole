c9cc98bac235e06e1562a38d6a2162d5f4ce6322
Johnny Marshall <the.mighty.git@gmail.com>
2023-07-26 19:31:22 +0100

    added item tests

---
+++ internal/rssole/item_test.go
@@ -0,0 +1,81 @@
+package rssole
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/mmcdole/gofeed"
+)
+
+func TestSummary_TruncateAt200(t *testing.T) {
+	w := wrappedItem{
+		Item: &gofeed.Item{
+			Description: strings.Repeat("x", 300),
+		},
+	}
+
+	s := w.Summary()
+
+	if len(s) != 200 {
+		t.Fatal("summary not truncted to 200")
+	}
+}
+
+func TestSummary_BlankIfSummaryIdenticalToTitle(t *testing.T) {
+	w := wrappedItem{
+		Item: &gofeed.Item{
+			Title:       "These are the same",
+			Description: "These are the same",
+		},
+	}
+
+	s := w.Summary()
+
+	if s != "" {
+		t.Fatal("summary was not blanked when identical to title")
+	}
+}
+
+func TestSummary_BlankIfSummaryAURL(t *testing.T) {
+	w := wrappedItem{
+		Item: &gofeed.Item{
+			Description: "http://example.com",
+		},
+	}
+
+	s := w.Summary()
+
+	if s != "" {
+		t.Fatal("summary was not blanked when a url")
+	}
+}
+
+func TestDescription_HtmlSanitised(t *testing.T) {
+	w := wrappedItem{
+		Item: &gofeed.Item{
+			Description: `
+<script>Should Be Deleted</script>
+<style>Should Be Deleted</style>
+<meta foo="Should Be Deleted">
+<iframe>Should Be Deleted</iframe>
+<a></a>
+<img >
+<svg >
+`,
+		},
+	}
+	expectedHtml := `<html><head>
+
+
+</head><body>
+<a target="_new"></a>
+<img style="max-width: 60%;"/>
+<svg style="max-width: 60%;">
+</svg></body></html>`
+
+	d := w.Description()
+
+	if d != expectedHtml {
+		t.Fatal("description not as expected. got", d, "expected:", expectedHtml)
+	}
+}
