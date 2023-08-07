1f0a61bdf8901ec5cb6ff7d691f465a861e1d197
Johnny Marshall <the.mighty.git@gmail.com>
2023-08-07 17:38:44 +0100

    move lastmodified code to own file

--- internal/rssole/feed.go
+++ internal/rssole/feed.go
@@ -14,25 +14,6 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
-// global last modified for use in Last-Modified/If-Modified-Since
-var (
-	muLastmodified sync.Mutex
-	lastmodified   time.Time
-)
-
-func updateLastmodified() {
-	muLastmodified.Lock()
-	lastmodified = time.Now()
-	muLastmodified.Unlock()
-}
-
-func getLastmodified() time.Time {
-	muLastmodified.Lock()
-	defer muLastmodified.Unlock()
-
-	return lastmodified
-}
-
 type feed struct {
 	URL        string        `json:"url"`
 	Name       string        `json:"name,omitempty"`     // optional override name
