72ae458466386167a3464b13224744df013f440b
Johnny <the.mighty.git@gmail.com>
2023-08-28 10:05:27 +0100

    use last-modified and etag when fetching feeds (#123)
    * use last-modified and etag when fetching feeds (good citizen), fixes #16

--- internal/rssole/feed.go
+++ internal/rssole/feed.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"sort"
 	"strings"
@@ -28,6 +30,30 @@ type feed struct {
 	mu           sync.RWMutex
 	wrappedItems []*wrappedItem
 	log          *slog.Logger
+
+	eTag         string
+	lastModified time.Time
+}
+
+var (
+	ErrNotModified = errors.New("not modified")
+
+	gmtTimeZoneLocation *time.Location
+
+	httpClientTimeout = 30 * time.Second
+
+	httpClient = &http.Client{
+		Timeout: httpClientTimeout,
+	}
+)
+
+func init() {
+	loc, err := time.LoadLocation("GMT")
+	if err != nil {
+		panic(err)
+	}
+
+	gmtTimeZoneLocation = loc
 }
 
 const maxRecentLogLines = 30
@@ -105,10 +131,7 @@ func (f *feed) Items() []*wrappedItem {
 }
 
 func (f *feed) Update() error {
-	var (
-		err  error
-		feed *gofeed.Feed
-	)
+	var feed *gofeed.Feed
 
 	fp := gofeed.NewParser()
 
@@ -128,9 +151,59 @@ func (f *feed) Update() error {
 		}
 	} else {
 		f.log.Info("Fetching and parsing feed", "url", f.URL)
-		feed, err = fp.ParseURL(f.URL)
+
+		req, err := http.NewRequest(http.MethodGet, f.URL, nil)
+		if err != nil {
+			return fmt.Errorf("cannot create new request: %w", err)
+		}
+		req.Header.Set("User-Agent", "Gofeed/1.0")
+
+		if f.eTag != "" {
+			req.Header.Set("If-None-Match", fmt.Sprintf(`"%s"`, f.eTag))
+		}
+
+		req.Header.Set("If-Modified-Since", f.lastModified.In(gmtTimeZoneLocation).Format(time.RFC1123))
+
+		resp, err := httpClient.Do(req)
+
+		if err != nil {
+			return fmt.Errorf("unable to do request: %w", err)
+		}
+
+		if resp != nil {
+			defer func() {
+				ce := resp.Body.Close()
+				if ce != nil {
+					err = ce
+				}
+			}()
+		}
+
+		if resp.StatusCode == http.StatusNotModified {
+			return ErrNotModified
+		}
+
+		if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+			return gofeed.HTTPError{
+				StatusCode: resp.StatusCode,
+				Status:     resp.Status,
+			}
+		}
+
+		feed, err = fp.Parse(resp.Body)
 		if err != nil {
 			return fmt.Errorf("rss parseurl %s %w", f.URL, err)
+		}
+
+		if eTag := resp.Header.Get("Etag"); eTag != "" {
+			f.eTag = eTag
+		}
+
+		if lastModified := resp.Header.Get("Last-Modified"); lastModified != "" {
+			parsed, err := time.ParseInLocation(time.RFC1123, lastModified, gmtTimeZoneLocation)
+			if err == nil {
+				f.lastModified = parsed
+			}
 		}
 	}
 
